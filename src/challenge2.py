65b1ea34e05a36fa5366fde5e2380efbc9347fb8
ioannakok <ioannakok@hotmail.com>
2018-06-14 00:35:43 +0100

    Make sure strings have same length

--- src/challenge2.py
+++ src/challenge2.py
@@ -5,5 +5,9 @@ def convert_hex_string_to_int(string):
 
 
 def xor_strings(str1, str2):
-    return hex(convert_hex_string_to_int(str1) ^ convert_hex_string_to_int(str2))
+    if len(str1) == len(str2):
+        return hex(convert_hex_string_to_int(str1) ^ convert_hex_string_to_int(str2))
+    else:
+        return
+
 
--- src/test_set1.py
+++ src/test_set1.py
@@ -22,6 +22,11 @@ class TestSetOne(unittest.TestCase):
         self.assertEqual(challenge2.convert_hex_string_to_int("f"), 15)
         self.assertEqual(challenge2.convert_hex_string_to_int("5b"), 91)
 
+    def test_should_return_none_if_strings_do_not_have_same_length(self):
+        string1 = "1c011100"
+        string2 = "686974207468652"
+        self.assertEqual(challenge2.xor_strings(string1, string2), None)
+
     def test_should_produce_xor_combination_of_strings(self):
         string1 = "1c0111001f010100061a024b53535009181c"
         string2 = "686974207468652062756c6c277320657965"
