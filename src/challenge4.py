7b773f82096b677bf7f57e6206163bb4a73e5e74
ioannakok <ioannakok@hotmail.com>
2018-07-28 22:31:47 +0100

    Detect single-character XOR

---
+++ src/challenge4.py
@@ -0,0 +1,14 @@
+from challenge3 import find_key
+
+
+def find_encrypted_message():
+    counter = 1
+    with open('../data/challenge4-data.txt') as file:
+        for line in file:
+            message = {"No": counter, "encrypted": line, "decrypted": find_key(line.rstrip())}
+            counter += 1
+            print(message)
+
+
+find_encrypted_message()
+
